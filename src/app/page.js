bc3e22fe7da5a43a81befa94553136da1c4bdc26
risky saputra <rizky.saputra@hiaria.id>
2023-10-19 13:16:42 +0700

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Calendar from "@components/Calendar";
 
 export default function Home() {
   return (
-    <main className="px-[100px]">
+    <main className="px-[100px] min-h-screen">
       <Calendar />
     </main>
   );
